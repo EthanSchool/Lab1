b15d9bb322bd54ba7e4ecb42d9067f89919b0153
EthanSchool <35495731+EthanSchool@users.noreply.github.com>
2018-01-16 09:48:26 -0600

    Initial Commit

---
+++ DriversExam/FancyConsole.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DriversExam {
+	public static class FancyConsole {
+		public static char[] PromptArray(int values, char[] validValues) {
+			ClearCurrentLine();
+
+			char[] data = new char[values];
+
+			Console.Write(string.Join(',', data).Replace('\0', '_'));
+			Console.SetCursorPosition(0, Console.CursorTop);
+
+			while(data[values - 1] == '\0') {
+				ConsoleKeyInfo input = Console.ReadKey(true);
+				if(input.Key == ConsoleKey.Escape)
+					return null;
+				HandleKeyInput(data, validValues, input);
+			}
+
+			Console.WriteLine();
+
+			return data;
+		}
+
+		private static void ClearCurrentLine() {
+			int currentLine = Console.CursorTop;
+			Console.SetCursorPosition(0, Console.CursorTop);
+			Console.Write(new string(' ', Console.WindowWidth));
+			Console.SetCursorPosition(0, currentLine);
+		}
+
+		private static void HandleKeyInput(char[] data, IEnumerable<char> validValues, ConsoleKeyInfo input) {
+			char key = char.ToUpper(input.KeyChar);
+			int found = Array.IndexOf(data, '\0');
+
+			if(input.Key == ConsoleKey.Backspace && found != 0) {
+				data[found - 1] = '\0';
+
+				Console.SetCursorPosition(0, Console.CursorTop);
+				Console.Write(string.Join(',', data).Replace('\0', '_'));
+				Console.SetCursorPosition((found - 1) * 2, Console.CursorTop);
+			} else {
+				if(!validValues.Contains(key)) return;
+				data[found] = key;
+
+				Console.SetCursorPosition(0, Console.CursorTop);
+				Console.Write(string.Join(',', data).Replace('\0', '_'));
+				Console.SetCursorPosition((found + 1) * 2, Console.CursorTop);
+			}
+		}
+	}
+}
---
+++ DriversExam/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace DriversExam {
+	internal static class Program {
+		private static readonly char[] Answers = "BDAACABACDBCDADCCBDA".ToCharArray();
+
+		private static void Main() {
+			Console.WriteLine("Press Esc to exit");
+			while(true) {
+				char[] studentAnswers = FancyConsole.PromptArray(20, new[] {'A', 'B', 'C', 'D'});
+
+				if(studentAnswers == null)
+					return;
+				int[] wrong = Answers.Select((value, index) => new {value, index})
+					.Where(x => x.value != studentAnswers[x.index])
+					.Select(x => x.index + 1)
+					.ToArray();
+				int correct = 20 - wrong.Length;
+
+				string t = correct / studentAnswers.Length > .75f ? "Passed" : "Failed";
+				Console.WriteLine($"Student {t}, {correct}/{studentAnswers.Length} correct, Incorrect Questions: {string.Join(',', wrong)}");
+			}
+		}
+	}
+}
